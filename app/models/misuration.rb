c8a5f8f5202cf97b7156ee4632b270ecbb07ab41
Filippo Luppi <filippoluppi@MacBook-Pro-di-Filippo.local>
2020-04-08 15:41:31 +0200

    definizione model misurations

--- app/models/misuration.rb
+++ app/models/misuration.rb
@@ -1,3 +1,11 @@
 class Misuration < ApplicationRecord
+	validates :mac, :value, :unit, presence: true
+	validates :value, numericality: true
+	validate :mac_presence
+
+def mac_presence
+  return true if Sensors.where(mac: self.mac).exists?
+end
+
 	belongs_to :sensor
 end
