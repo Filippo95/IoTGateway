c262f9d739fad4805a492f2e01b1051f4e268bea
Marco <marco.andreotti94@gmail.com>
2020-04-13 15:44:44 +0200

    Modifiche varie

--- app/models/sensors_group.rb
+++ app/models/sensors_group.rb
@@ -1,3 +1,8 @@
 class SensorsGroup < ApplicationRecord
+
+	validates :nome, :id_user_group, presence: true
+	#Aggiungere parte relativa all'user group cioè controllare che id del gruppo sia presente
+	
+
 	has_many :sensor_group_subscriptions
 end
--- app/models/sensors_group_subscription.rb
+++ app/models/sensors_group_subscription.rb
@@ -1,4 +1,8 @@
 class SensorsGroupSubscription < ApplicationRecord
+
+	validates :id_sensor_group, :id_sensor, presence: true, numericality: {only_float: true}
+	#Aggiungere
+
 	belongs_to :sensor 
 	belongs_to :sensor_group
 	
