c2f5e80133c22d5c4336d8fa3e73ab7ae821eb61
michimaestri <maestri612@gmail.com>
2020-04-13 15:05:10 +0200

    validates su sensor

--- app/models/misuration_subscription.rb
+++ app/models/misuration_subscription.rb
@@ -1,4 +1,6 @@
 class MisurationSubscription < ApplicationRecord
+	validates :mac,:user_id,presence:true
+
 	belongs_to :user
 	belongs_to :sensor
 end
--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,4 +1,12 @@
 class Sensor < ApplicationRecord
+	validates :mac,:url,:tipo,:latitudine,:longitudine,:unit_misura,presence:true
+	validates :mac, uniqueness:true
+	#converte un intero in float 7->7.0
+	validates :latitudine,:longitudine,:downtime_to_alarm, numericality: {only_float: true}
+
+	#come fare il validates su last_firware_update date?
+	validates :public, inclusion: { in: [ true, false ] }
+
 	has_many :misuration_subscriptions
 	has_many :misurations 
 	has_many :sensor_group_subscriptions
