6b5412ff68dc6b349045a8d9811d9ad90d461d26
michele <maestri612@gmail.com>
2021-02-01 15:18:50 +0100

    controller test misuration sensor e misuration_subscription

--- test/controllers/misuration_subscriptions_controller_test.rb
+++ test/controllers/misuration_subscriptions_controller_test.rb
@@ -1,44 +1,69 @@
 require 'test_helper'
 
 class MisurationSubscriptionsControllerTest < ActionDispatch::IntegrationTest
-  setup do
+  setup :initialize_log_in
+  def initialize_log_in
+    @sensor = sensors(:one)
+    @user = users(:user_001)
     @misuration_subscription = misuration_subscriptions(:one)
   end
+ 
 
   test "should get index" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     get misuration_subscriptions_url
     assert_response :success
   end
 
   test "should get new" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     get new_misuration_subscription_url
     assert_response :success
   end
 
   test "should create misuration_subscription" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     assert_difference('MisurationSubscription.count') do
-      post misuration_subscriptions_url, params: { misuration_subscription: { mac: @misuration_subscription.mac, user_id: @misuration_subscription.user_id } }
+      post misuration_subscriptions_url, params: { misuration_subscription: {id: @misuration_subscription.id, sensor_id: @misuration_subscription.sensor_id, user_id: @misuration_subscription.user_id } }
     end
 
     assert_redirected_to misuration_subscription_url(MisurationSubscription.last)
   end
 
   test "should show misuration_subscription" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     get misuration_subscription_url(@misuration_subscription)
     assert_response :success
   end
 
   test "should get edit" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     get edit_misuration_subscription_url(@misuration_subscription)
     assert_response :success
   end
 
   test "should update misuration_subscription" do
-    patch misuration_subscription_url(@misuration_subscription), params: { misuration_subscription: { mac: @misuration_subscription.mac, user_id: @misuration_subscription.user_id } }
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
+    patch misuration_subscription_url(@misuration_subscription), params: { misuration_subscription: { id: @misuration_subscription.id ,sensor_id: @misuration_subscription.sensor_id, user_id: @misuration_subscription.user_id } }
     assert_redirected_to misuration_subscription_url(@misuration_subscription)
   end
 
   test "should destroy misuration_subscription" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     assert_difference('MisurationSubscription.count', -1) do
       delete misuration_subscription_url(@misuration_subscription)
     end
--- test/controllers/misurations_controller_test.rb
+++ test/controllers/misurations_controller_test.rb
@@ -1,21 +1,33 @@
 require 'test_helper'
 
 class MisurationsControllerTest < ActionDispatch::IntegrationTest
-  setup do
+  setup :initialize_log_in
+  def initialize_log_in
+    @sensor = sensors(:one)
     @misuration = misurations(:one)
   end
+    
 
   test "should get index" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     get misurations_url
     assert_response :success
   end
 
   test "should get new" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     get new_misuration_url
     assert_response :success
   end
 
   test "should create misuration" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     assert_difference('Misuration.count',1) do
       post misurations_url, params: { misuration: { mac: @misuration.mac, unit: @misuration.unit, value: @misuration.value } }
     end
@@ -24,21 +36,33 @@ class MisurationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show misuration" do
-    get misuration_url(@misuration)
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
+    get misuration_path(@misuration)
     assert_response :success
   end
 
   test "should get edit" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     get edit_misuration_url(@misuration)
     assert_response :success
   end
 
   test "should update misuration" do
-    patch misuration_url(@misuration), params: { misuration: { mac: @misuration.mac, unit: @misuration.unit, value: @misuration.value } }
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
+    patch misuration_url(@misuration), params: { misuration: { mac: @misuration.mac, unit: @misuration.unit, value: @misuration.value, id: @misuration.id} }
     assert_redirected_to misuration_url(@misuration)
   end
 
   test "should destroy misuration" do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_path
     assert_difference('Misuration.count', -1) do
       delete misuration_url(@misuration)
     end
